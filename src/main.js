1ab4d48179e2e5905ff970dcea1b3c3243ef0c58
Sergey Novikov <69253755+Serj-New@users.noreply.github.com>
2023-11-06 19:18:03 +0200

    UPD header

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-menu-open]'), 
-    //   data-menu-open - Атрибут на кнопку відкриття мєню
       closeModalBtn: document.querySelector('[data-menu-close]'),
-    //   data-menu-close - Атрибут на кнопку закриття мєню
       modal: document.querySelector('[data-menu]'),
-    //   data-menu - Атрибут на div, в якому зроблеме мєню
     };
   
     refs.openModalBtn.addEventListener('click', toggleModal);
